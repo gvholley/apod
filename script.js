fd27d22fe06be32cb5f3c46a785b7ec96991047e
Grant Holley <gvholley@gmail.com>
2021-03-05 21:23:17 +0900

    JS now displays card

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const resultsNav = document.getElementById('resultsNav');
 const favoritesNav = document.getElementById('favoritesNav');
 const imagesContainer = document.querySelector('.images-container');
-const saveConfirmed = dcoument.querySelector('.save-confirmed');
+const saveConfirmed = document.querySelector('.save-confirmed');
 const loader = document.querySelector('.loader');
 
 //NASA API
@@ -11,8 +11,43 @@ const apiURL = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${co
 
 let resultsArray =[];
 
+//generates card
 function updateDOM() {
-
+  resultsArray.forEach((result) => {
+    const card = document.createElement('div');
+    card.classList.add('card')
+    const link = document.createElement('a');
+    link.href = result.hdurl;
+    link.title = 'View Full Image';
+    link.target = '_blank';
+    const image = document.createElement('img');
+    image.src = result.url;
+    image.alt = 'NASA APOD';
+    image.loading = 'lazy';
+    image.classList.add('card-img-top');
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+    const cardTitle = document.createElement('h5');
+    cardTitle.classList.add('card-title');
+    cardTitle.textContent = result.title;
+    const saveText = document.createElement('p');
+    saveText.classList.add('clickable');
+    saveText.textContent = 'Add to Favorites';
+    const cardText = document.createElement('p');
+    cardText.textContent = result.explanation;
+    const footer = document.createElement('small');
+    footer.classList.add('text-muted');
+    const date = document.createElement('strong');
+    date.textContent = result.date;
+    const copyrightResult = result.copyright === undefined ? '' : result.copyright;
+    const copyright = document.createElement('span');
+    copyright.textContent = `${copyrightResult}`;
+    footer.append(date, copyright);
+    cardBody.append(cardTitle, saveText, cardText, footer);
+    link.appendChild(image);
+    card.append(link, cardBody);
+    imagesContainer.appendChild(card);
+  });
 }
 
 // Get Images from NASA API
